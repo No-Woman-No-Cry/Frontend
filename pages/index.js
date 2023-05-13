7fd3c97ff43a995dbd6a0554833d9e01ab48d485
Erik Mikael <erikfreecss@gmail.com>
2023-05-13 14:04:02 +0700

    test init

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.scss'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <p>test</p>
-
-
-  )
+  return <p>done</p>;
 }
